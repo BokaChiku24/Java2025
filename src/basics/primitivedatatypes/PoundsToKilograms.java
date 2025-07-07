2b65047d655a083374ad57d7912f75128106210c
kunalchavan <kunal24chavan@gmail.com>
2025-07-08 00:12:57 +0530

    Java Basics

---
+++ src/basics/primitivedatatypes/PoundsToKilograms.java
@@ -0,0 +1,9 @@
+package basics.primitivedatatypes;
+
+public class PoundsToKilograms {
+    public static void main(String[] args) {
+        float pounds = 200.24f;
+        double kilogram = (pounds * 0.45359237);
+        System.out.println("Pounds to kilogram is: " + kilogram);
+    }
+}
