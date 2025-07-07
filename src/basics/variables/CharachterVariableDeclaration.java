dbcae3b570f2b15d74c788398de230fe5d31630a
kunalchavan <kunal24chavan@gmail.com>
2025-07-08 00:22:05 +0530

    Java Basics

---
+++ src/basics/variables/CharachterVariableDeclaration.java
@@ -0,0 +1,17 @@
+package basics.variables;
+
+public class CharachterVariableDeclaration {
+    public static void main(String[] args) {
+        // a literal character
+        char myChar = 'K';
+        // a unicode value
+        char myUnicodeValue = '\u0044';
+        // a integer value
+        char myIntegerValueChar = 68;
+
+        System.out.println("Literal character is: " + myChar);
+        System.out.println("Unicode character is: " + myUnicodeValue);
+        System.out.println("Integer value character is: " + myIntegerValueChar);
+
+    }
+}
