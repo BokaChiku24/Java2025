2d17099206931bacec064be1883704929d16c520
kunalchavan <kunal24chavan@gmail.com>
2025-07-12 15:49:45 +0530

    New Package Added

---
+++ src/methods/Methods.java
@@ -0,0 +1,53 @@
+package methods;
+
+public class Methods {
+    public static void main(String[] args) {
+        boolean gameOver = true;
+        int score = 800;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+        int finalScore = score;
+
+        calculateScore(true, 800, 5, bonus = 100);
+       /* boolean newGameOver = true;
+        int newScore = 10000;
+        int newLevelCompleted = 8;
+        int newBonus = 200;
+
+        int newFinalScore = newScore;
+
+        if(newGameOver){
+            newFinalScore += (newLevelCompleted * newBonus);
+            // shorthand = finalScore = finalScore + (levelCompleted * bonus);
+            System.out.println("Your final score was: " + newFinalScore);
+        }*/
+
+        gameOver = true;
+        score = 10000;
+        levelCompleted = 8;
+        bonus = 200;
+
+        calculateScore(true, 10000, 8, 200);
+    }
+
+    public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+
+        int finalScore = score;
+
+        /*
+        if(gameOver == true){
+            finalScore += (levelCompleted * bonus);
+            // shorthand = finalScore = finalScore + (levelCompleted * bonus);
+            System.out.println("Your final score was: " + finalScore);
+        }
+        */
+
+        // same as line number 13
+        if (gameOver) {
+            finalScore += (levelCompleted * bonus);
+            // shorthand = finalScore = finalScore + (levelCompleted * bonus);
+            System.out.println("Your final score was: " + finalScore);
+        }
+    }
+}
