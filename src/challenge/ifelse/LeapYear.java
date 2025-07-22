3ff6b3c5cd9f26752ae7913f7da88b7edc147e39
kunalchavan <kunal24chavan@gmail.com>
2025-07-23 03:25:56 +0530

    Java Logic

--- src/challenge/ifelse/LeapYear.java
+++ src/challenge/ifelse/LeapYear.java
@@ -2,7 +2,7 @@ package challenge.ifelse;
 
 public class LeapYear {
     public static void main(String[] args){
-
+        System.out.println(isLeapYear(2001));
     }
     public static boolean isLeapYear(int year) {
 
