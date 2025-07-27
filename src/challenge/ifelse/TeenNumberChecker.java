cfa1ca29a5c93987753d7fba5bc3eee2a4200821
kunalchavan <kunal24chavan@gmail.com>
2025-07-27 19:48:42 +0530

    Java Logic If Else

---
+++ src/challenge/ifelse/TeenNumberChecker.java
@@ -0,0 +1,54 @@
+package challenge.ifelse;
+/*
+Devising A Teen Number Checker For Mastery Of Conditional Range Checking
+We'll say that a number is "teen" if it is in the range 13 -19 (inclusive).
+Write a method named hasTeen with 3 parameters of type int.
+The method should return boolean and it needs to return true if one of the parameters is in range 13(inclusive) - 19 (inclusive). Otherwise return false.
+
+EXAMPLES OF INPUT/OUTPUT:
+hasTeen(9, 99, 19);  should return true since 19 is in range 13 - 19
+hasTeen(23, 15, 42);  should return true since 15 is in range 13 - 19
+hasTeen(22, 23, 34);  should return false since numbers 22, 23, 34 are not in range 13-19
+
+Write another method named isTeen with 1 parameter of type int.
+The method should return boolean and it needs to return true if the parameter is in range 13(inclusive) - 19 (inclusive). Otherwise return false.
+EXAMPLES OF INPUT/OUTPUT:
+isTeen(9);  should return false since 9 is in not range 13 - 19
+isTeen(13);  should return true since 13 is in range 13 - 19
+ */
+public class TeenNumberChecker {
+    public static void main(String... args) {
+        System.out.println(hasTeen(13, 20, 19)); // should return true
+        System.out.println(hasTeen(22, 23, 34)); // should return false
+        System.out.println(isTeen(13)); // should return true
+        System.out.println(isTeen(22)); // should return false
+    }
+
+    public static boolean hasTeen(int num, int num2, int num3) {
+        return (num >= 13 && num <= 19) || (num2 >= 13 && num2 <= 19) || (num3 >= 13 && num3 <= 19);
+    }
+
+    public static boolean isTeen(int num) {
+        return num >= 13 && num <= 19;
+    }
+
+    /*
+
+    public static boolean hasTeen(int num1, int num2, int num3) {
+        if ((num1 >= 13 && num1 <= 19) || (num2 >= 13 && num2 <= 19) || (num3 >= 13 && num3 <= 19)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static boolean isTeen(int num) {
+        if (num >= 13 && num <= 19) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    // The above methods can be simplified to the following:
+     */
+}
