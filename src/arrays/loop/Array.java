cbcc92f4327557a7c20f3275b37f157e09bfa7c1
kunalchavan <kunal24chavan@gmail.com>
2025-09-12 02:15:07 +0530

    OOPS

---
+++ src/arrays/loop/Array.java
@@ -0,0 +1,11 @@
+package arrays.loop;
+
+public class Array {
+    public static void main(String[] args) {
+        int[] newArray;
+        newArray = new int[5];
+        for(int i = 0; i < newArray.length; i++){
+            System.out.print(newArray[i] + "\t");
+        }
+    }
+}
